6acac05ea58c23ba98340cd45a2b535577985439
domaa98 <modomaa61@gmial.com>
2025-02-09 06:38:16 +0200

    last version

--- src/component/clientsReview/ClientsReview.jsx
+++ src/component/clientsReview/ClientsReview.jsx
@@ -1,11 +1,11 @@
 
-import { useState } from "react";
 import './clientsReview.css'
 import client5 from '../../assets/photo5.png'
 import client2 from '../../assets/client2.jpg'
 import photo7 from '../../assets/photo7.png'
 import { FaArrowLeftLong, FaArrowRight } from 'react-icons/fa6';
 import { FaStar } from "react-icons/fa6";
+import { useRef } from 'react';
 
 
 const clients = [
@@ -13,63 +13,65 @@ const clients = [
       id: 1,
       image: client5,
       name: 'ندي احمد',
-      description: 'تجربتي كانت ممتازة! الطلب وصل سريعًا، وكل شيء كان بجودة عالية. بالتأكيد سأطلب مرة أخرى.',
+      description: 'تجربتي كانت ممتازة! الطلب وصل سريعًا، وكل شيء كان بجودة عالية. بالتأكيد سأطلب مرة أخرى',
     },
     {
       id: 2,
       image: client2,
       name: 'محمد السيد',
-      description: 'الفريق متعاون جداً، والمنتجات دائماً طازجة ومطابقة للوصف. أشعر بالثقة في التعامل معكم.',
+      description: 'الفريق متعاون جداً، والمنتجات دائماً طازجة ومطابقة للوصف. أشعر بالثقة في التعامل معكم',
     },
     {
       id: 3,
       image: photo7,
       name: 'فاروق',
-      description: 'الخدمة ممتازة، والتوصيل سريع. أحببت العناية بالتفاصيل وجودة المنتجات. شكراً لكم!',
+      description: 'الخدمة ممتازة، والتوصيل سريع. أحببت العناية بالتفاصيل وجودة المنتجات. شكراً لكم',
     },
     
   ];
 
 const ClientsReview = () => {
 
-        const cardsPerSlide = 3;
-        const [currentIndex, setCurrentIndex] = useState(0);
-      
-        const nextSlide = () => {
-          if (currentIndex + cardsPerSlide < clients.length) {
-            setCurrentIndex(currentIndex + cardsPerSlide);
-          }
-        };
-      
-        const prevSlide = () => {
-          if (currentIndex - cardsPerSlide >= 0) {
-            setCurrentIndex(currentIndex - cardsPerSlide);
-          }
-        };
-    
-        const visibleCards = clients.slice(currentIndex, currentIndex + cardsPerSlide);
-    
+  const containerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollBy({
+        left: -150, 
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollBy({
+        left: 150, 
+        behavior: "smooth",
+      });
+    }
+  };
 
   return (
    <div className="ourClientsReview">
        <div className="top-section">
          <div className="title">شهادات العملاء</div>
-         {clients.length > cardsPerSlide && (
+         
           <div className="buttons">
-            <button onClick={prevSlide} disabled={currentIndex === 0}>
+            <button onClick={scrollLeft} /*disabled={currentIndex === 0}*/>
               <FaArrowLeftLong />
             </button>
             <button
-              onClick={nextSlide}
-              disabled={currentIndex + cardsPerSlide >= clients.length}
+              onClick={scrollRight}
+              /*disabled={currentIndex + cardsPerSlide >= clients.length}*/
             >
               <FaArrowRight />
             </button>
           </div>
-        )}
+        
        </div>
-       <div className="slider">
-         {visibleCards.map((card) => (
+       <div className="slider" ref={containerRef}>
+         {clients.map((card) => (
            <div key={card.id} className="card">
              <img
              className="card-image"
